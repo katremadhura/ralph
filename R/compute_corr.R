15cf25f10145de9dc551507d3db67937f89461f1
katremadhura <katre.madhura@gmail.com>
2021-08-31 15:38:06 -0500

    the error, warning and note free package

---
+++ R/compute_corr.R
@@ -0,0 +1,29 @@
+#' Computes a tidy correlation
+#'
+#' @param data input dataset
+#' @param var1 name of variable 1 (unquoted)
+#' @param var2 name of variable 2 (unquoted)
+#'
+#' @return a tibble with Pearson correlation and p-value
+#' @export
+#'
+#' @examples
+#' compute_corr(data = faithful,var1 = eruptions, var2 = waiting)
+#'
+#' @importFrom rlang .data
+compute_corr <- function(data, var1, var2){
+
+  # compute correlation ----
+  stats::cor.test(
+    x = data %>% dplyr::pull({{var1}}),
+    y = data %>% dplyr::pull({{var2}})
+  ) %>%
+    # tidy up results ----
+  broom::tidy() %>%
+    # retain and rename relevant bits ----
+  dplyr::select(
+    correlation = .data$estimate,
+    pval = .data$p.value
+  )
+
+}
