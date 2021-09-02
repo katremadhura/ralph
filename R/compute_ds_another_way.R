a6cb1d0168362ba0625a5b794b5908a012a3d7fd
katremadhura <katre.madhura@gmail.com>
2021-09-01 19:20:45 -0500

    fixing the errors introduced by incorrect referencing of columns

--- R/compute_ds_another_way.R
+++ R/compute_ds_another_way.R
@@ -13,10 +13,10 @@
 #' @importFrom rlang .data
 compute_ds_another_way <- function(data,var1,var2){
   # compute and store statistics
-  average <-  c(mean(data$var1,na.rm=TRUE), mean(data$var2,na.rm=TRUE))
-  minimum <- c(min(data$var1,na.rm=TRUE),min(data$var2,na.rm=TRUE))
-  maximum <- c(max(data$var1,na.rm=TRUE),max(data$var2,na.rm=TRUE))
-  stdev <- c(stats::sd(data$var1,na.rm=TRUE),stats::sd(data$var2,na.rm=TRUE))
+  average <-  c(mean(data[[var1]],na.rm=TRUE), mean(data[[var2]],na.rm=TRUE))
+  minimum <- c(min(data[[var1]],na.rm=TRUE),min(data[[var2]],na.rm=TRUE))
+  maximum <- c(max(data[[var1]],na.rm=TRUE),max(data[[var2]],na.rm=TRUE))
+  stdev <- c(stats::sd(data[[var1]],na.rm=TRUE),stats::sd(data[[var2]],na.rm=TRUE))
 
   dataSummary <- rbind(average,minimum,maximum,stdev)
   colnames(dataSummary) <- colnames(data)
